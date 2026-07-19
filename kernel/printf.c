abb0f3fd6972754f6503b98efcb7a8f7b9fea954
miyamaenagisa <tianzhongxiaoji75@gmail.com>
2026-07-19 18:32:46 +0900

    add vga output

--- kernel/printf.c
+++ kernel/printf.c
@@ -1,1 +1,12 @@
 #include <stdarg.h>
+
+volatile unsigned short *vga =
+    (volatile unsigned short *)0xB8000;
+
+unsigned int cursor = 0;
+
+
+void putchar(char c)
+{
+    vga[cursor++] = (unsigned short)c | 0x0F00;
+}
