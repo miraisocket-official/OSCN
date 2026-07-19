3e75550163f376ada65e428bed95936600479ba5
miyamaenagisa <tianzhongxiaoji75@gmail.com>
2026-07-19 18:55:02 +0900

    add printf support

--- kernel/printf.c
+++ kernel/printf.c
@@ -11,16 +11,46 @@ void putchar(char c)
     vga[cursor++] = (unsigned short)c | 0x0F00;
 }
 
+
 void print_string(const char *s)
 {
-    while (*s)
+    while(*s)
     {
         putchar(*s++);
     }
 }
 
-void printf(const char *fmt)
-{
-    print_string(fmt);
-}
 
+void printf(const char *fmt,...)
+{
+    va_list args;
+
+    va_start(args,fmt);
+
+    while(*fmt)
+    {
+        if(*fmt == '%')
+        {
+            fmt++;
+
+            if(*fmt == 's')
+            {
+                char *s = va_arg(args,char *);
+                print_string(s);
+            }
+            else if(*fmt == 'c')
+            {
+                char c = va_arg(args,int);
+                putchar(c);
+            }
+        }
+        else
+        {
+            putchar(*fmt);
+        }
+
+        fmt++;
+    }
+
+    va_end(args);
+}
